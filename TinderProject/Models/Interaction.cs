85c330f5179118d3f32d884a36b3312aac63e770
Henrik1995a@live.se <henrik1995a@live.se>
2023-05-02 13:28:52 +0200

    123

---
+++ TinderProject/Models/Interaction.cs
@@ -0,0 +1,13 @@
+﻿namespace TinderProject.Models
+{
+    public class Interaction
+    {
+        public int Id { get; set; }
+        public int LikerId { get; set; }
+        public int LikedId { get; set; }
+        public DateTime DateLiked { get; set; }
+
+        public User Liker { get; set; }
+        public User Liked { get; set; }
+    }
+}
---
+++ TinderProject/Models/Match.cs
@@ -0,0 +1,13 @@
+﻿namespace TinderProject.Models
+{
+    public class Match
+    {
+        public int MatchId { get; set; }
+        public int User1Id { get; set; }
+        public int User2Id { get; set; }
+        public DateTime MatchDate { get; set; }
+
+        public User User1 { get; set; }
+        public User User2 { get; set; }
+    }
+}
