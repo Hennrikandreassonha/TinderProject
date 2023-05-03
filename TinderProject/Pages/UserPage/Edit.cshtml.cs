c3fb6678d5c32ad92407404fe8fa49470b83f630
mangealonso <112272972+mangealonso@users.noreply.github.com>
2023-05-03 13:03:39 +0200

    Start code for index and edit pages

--- TinderProject/Pages/UserPage/Edit.cshtml.cs
+++ TinderProject/Pages/UserPage/Edit.cshtml.cs
@@ -5,13 +5,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TinderProject.Repositories.Repositories_Interfaces;
 
 namespace TinderProject.Pages.UserPage
 {
     public class EditModel : PageModel
     {
+        private readonly IUserRepository _userRepository;
+
+        public EditModel(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
         public void OnGet()
         {
+            var loggedInUser = _userRepository.GetLoggedInUser();
         }
     }
 }
--- TinderProject/Pages/UserPage/Index.cshtml.cs
+++ TinderProject/Pages/UserPage/Index.cshtml.cs
@@ -5,13 +5,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TinderProject.Repositories.Repositories_Interfaces;
 
 namespace TinderProject.Pages.UserPage
 {
     public class IndexModel : PageModel
     {
+        private readonly IUserRepository _userRepository;
+
+        public IndexModel(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
         public void OnGet()
         {
+            var loggedInUser = _userRepository.GetLoggedInUser();
         }
     }
 }
