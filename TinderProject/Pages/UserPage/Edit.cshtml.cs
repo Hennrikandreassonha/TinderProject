8e5dc2c183e6148888d7ba881ea2a93ccb526d8a
mangealonso <112272972+mangealonso@users.noreply.github.com>
2023-05-03 15:52:07 +0200

    Updated code for index and edit pages

--- TinderProject/Pages/UserPage/Edit.cshtml.cs
+++ TinderProject/Pages/UserPage/Edit.cshtml.cs
@@ -5,21 +5,57 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TinderProject.Data;
 using TinderProject.Repositories.Repositories_Interfaces;
 
 namespace TinderProject.Pages.UserPage
 {
-    public class EditModel : PageModel
-    {
-        private readonly IUserRepository _userRepository;
+	public class EditModel : PageModel
+	{
+		private readonly IUserRepository _userRepository;
+		private readonly AppDbContext _database;
 
-        public EditModel(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-        }
-        public void OnGet()
-        {
-            var loggedInUser = _userRepository.GetLoggedInUser();
-        }
-    }
+		public EditModel(IUserRepository userRepository, AppDbContext database)
+		{
+			_userRepository = userRepository;
+			_database = database;
+		}
+
+		public User User { get; set; }
+
+		public void OnGet()
+		{
+			var loggedInUser = _userRepository.GetLoggedInUser();
+
+			User = _database.Users.Find(loggedInUser.Id);
+		}
+
+		public IActionResult OnPost(User user)
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+
+			var loggedInUser = _userRepository.GetLoggedInUser();
+			var userToUpdate = _database.Users.Find(loggedInUser.Id);
+
+			if (userToUpdate == null)
+			{
+				return NotFound();
+			}
+
+			userToUpdate.FirstName = User.FirstName;
+			userToUpdate.LastName = User.LastName;
+			userToUpdate.DateOfBirth = User.DateOfBirth;
+			userToUpdate.Gender = User.Gender;
+			userToUpdate.Preference = User.Preference;
+			userToUpdate.Description = User.Description;
+
+			_database.Users.Update(userToUpdate);
+			_database.SaveChanges();
+
+			return RedirectToPage("/UserPage/Index");
+		}
+	}
 }
--- TinderProject/Pages/UserPage/Index.cshtml.cs
+++ TinderProject/Pages/UserPage/Index.cshtml.cs
@@ -5,21 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TinderProject.Data;
 using TinderProject.Repositories.Repositories_Interfaces;
 
 namespace TinderProject.Pages.UserPage
 {
-    public class IndexModel : PageModel
-    {
-        private readonly IUserRepository _userRepository;
+	public class IndexModel : PageModel
+	{
+		private readonly IUserRepository _userRepository;
+		private readonly AppDbContext _database;
 
-        public IndexModel(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-        }
-        public void OnGet()
-        {
-            var loggedInUser = _userRepository.GetLoggedInUser();
-        }
-    }
+		public IndexModel(IUserRepository userRepository, AppDbContext database)
+		{
+			_userRepository = userRepository;
+			_database = database;
+		}
+
+		public User User { get; set; }
+
+		public void OnGet()
+		{
+			var loggedInUser = _userRepository.GetLoggedInUser();
+
+			User = _database.Users.Find(loggedInUser.Id);
+		}
+	}
 }
