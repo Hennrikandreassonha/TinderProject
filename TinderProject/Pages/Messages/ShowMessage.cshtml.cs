4ce825655d28ac54b7feaf84cff420986e506dc6
Alexander Berg <112374693+bergalexander@users.noreply.github.com>
2023-05-04 11:02:02 +0200

    Azure and all message is shown in order

--- TinderProject/Pages/Messages/ShowMessage.cshtml.cs
+++ TinderProject/Pages/Messages/ShowMessage.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TinderProject.Data;
+using TinderProject.Models;
 using TinderProject.Repositories.Repositories_Interfaces;
 
 namespace TinderProject.Pages.Messages
@@ -12,27 +13,31 @@ namespace TinderProject.Pages.Messages
 
         public Message Message { get; set; }
         public User User { get; set; }
-        public List<Message> ReceivedMessages { get; set; }
+        public List<Message> Messages { get; set; }
 
         public MessegesModel(AppDbContext database, IUserRepository userRepository)
         {
             _database = database;
             _userRepository = userRepository;
-            ReceivedMessages = new List<Message>();
+            Messages = new List<Message>();
         }
 
+        //fixa så man kan se alla meddelanden
         public void OnGet()
         {
             var currentUser = _userRepository.GetLoggedInUser();
-            var findReceivedMessages = _database.Messages
-                .Include(m => m.User)
-                .Where(m => m.SentToId == currentUser.Id);
+            User = currentUser;
+            var messages = _database.Messages
+            .Include(m => m.User)
+            .Where(m => m.User.Id == User.Id || m.SentToId == User.Id)
+            .OrderBy(m => m.SentTime)
+            .ToList();
 
-            ReceivedMessages.AddRange(findReceivedMessages);
+            Messages.AddRange(messages);
         }
 
 
-        //Fixa detta
+
         public IActionResult OnPost(string message)
         {
             var sender = 0;
@@ -45,7 +50,7 @@ namespace TinderProject.Pages.Messages
                 sender = c.User.Id;
 
             }
-           
+
             var messagesToAdd = new Message
             {
                 SentMessage = message,
@@ -58,7 +63,7 @@ namespace TinderProject.Pages.Messages
             _database.Messages.Add(messagesToAdd);
             _database.SaveChanges();
 
-            return Page();
+            return RedirectToPage();
         }
     }
 }
