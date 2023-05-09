ac53a3ae54bc068dacd61d49ac69743327111fcf
Alexander Berg <112374693+bergalexander@users.noreply.github.com>
2023-05-09 14:52:48 +0200

    Delete update

---
+++ TinderProject/Pages/Messages/Delete.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TinderProject.Models;
+
+namespace TinderProject.Pages.Messages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AppDbContext _database;
+        private readonly IUserRepository _userRepository;
+        public User User { get; set; }
+        
+
+        public DeleteModel(AppDbContext database, IUserRepository userRepository)
+        {
+            _database = database;
+            _userRepository = userRepository;
+           User= new User();
+        }
+        public void OnGet(int userId)
+        {
+            User = _database.Users.Find(userId);
+            
+        }
+     
+        public IActionResult OnPost(int userId)
+        {
+            var currentUser = _userRepository.GetLoggedInUser();
+            var matches = _database.Matches
+                .Where(m => (m.User1Id == userId && m.User2Id == currentUser.Id) ||
+                            (m.User1Id == currentUser.Id && m.User2Id == userId))
+                            .ToList();
+
+            var messages = _database.Messages.
+                Where(m => (m.SentFromId == userId && m.SentToId == currentUser.Id) ||
+                           (m.SentToId == userId && m.SentFromId == currentUser.Id)).
+                ToList();
+
+            _database.Matches.RemoveRange(matches);
+            _database.Messages.RemoveRange(messages);
+
+            _database.SaveChanges();
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ TinderProject/Pages/Premium/ShowLikerInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TinderProject.Pages.Premium
+{
+    public class ShowLikerInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
